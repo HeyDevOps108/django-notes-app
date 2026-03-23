f1181e4b36b6bbafedd9884f29cf1eac023191f5
Vedant <Vedantarole18@gmail.com>
2026-03-23 23:07:22 +0530

    key and output

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,20 +1,20 @@
 output "notes_app_public_ip" {
   value = {
-    for key, instance in aws_aws_instance.notes-app : 
+    for key, instance in aws_instance.notes-app : 
     key => instance.public_ip
   }
 }
 
 output "notes_app_private_ip" {
   value = {
-    for key, instance in aws_aws_instance.notes-app :
+    for key, instance in aws_instance.notes-app :
     key => instance.private_ip
   }
 }
 
 output "notes_app_public_dns" {
   value = {
-    for key, instance in aws_aws_instance.notes-app :
+    for key, instance in aws_instance.notes-app :
     key => instance.public_dns
   }
 }
